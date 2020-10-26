fff9382f09b2f8a655c68822ccc7c65004ec78c4
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2020-10-26 10:27:16 +0530

    Hacking with swifui day 28 challenge completed

--- betterRest/ContentView.swift
+++ betterRest/ContentView.swift
@@ -10,45 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wakeUp = Date()
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
     
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+            Form {
+                Section {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(value: $coffeeAmount, in: 1...20) {
-                    if coffeeAmount == 1 {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
-                    .navigationBarTitle("BetterRest")
-                    .navigationBarItems(trailing: Button(action: calculateBedtime) {
+                
+                Section {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                .navigationBarTitle("BetterRest")
+                .navigationBarItems(trailing: Button(action: calculateBedtime) {
                     Text("Calculate")
                 })
-                
+                    
                     .alert(isPresented: $showingAlert) {
-                        Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                        Alert(title: Text(alertTitle).font(.largeTitle), message: Text(alertMessage).font(.title), dismissButton: .default(Text("OK")))
                 }
             }
         }
